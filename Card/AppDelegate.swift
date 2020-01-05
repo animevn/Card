987801425822895f114567b41d164d13e2044108
animevn <haanhth21@gmail.com>
2020-01-05 21:07:07 +0700

    add menuview

--- Card/AppDelegate.swift
+++ Card/AppDelegate.swift
@@ -9,13 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey:Any]?)->Bool{
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .orange
-        let menu = MenuController()
-        window?.rootViewController = menu
-        window?.makeKeyAndVisible()
-        
         return true
     }
 
--- Card/MenuController.swift
+++ Card/MenuController.swift
@@ -4,4 +4,21 @@ class MenuController:UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func bnEasy(_ sender: UIButton) {
+    }
+    
+    @IBAction func bnNormal(_ sender: UIButton) {
+    }
+    
+    @IBAction func bnHard(_ sender: UIButton) {
+    }
+    
+    @IBAction func bnSave(_ sender: UIButton) {
+    }
+    
+    @IBAction func bnCenter(_ sender: UIButton) {
+    }
+    
 }
