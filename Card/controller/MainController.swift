9e00fab06703bcd2fa994bf0183e655c953089bf
animevn <haanhth21@gmail.com>
2020-01-06 11:54:46 +0700

    add function calculate view

--- Card/controller/MainController.swift
+++ Card/controller/MainController.swift
@@ -16,6 +16,40 @@ class MainController:UIViewController{
         print("The class \(type(of: self)) was remove from memory")
     }
     
+    private func cardsPerLevel()->(column:CGFloat, row:CGFloat){
+        switch level! {
+        case .Easy:
+            return (3, 4)
+        case .Normal:
+            return (4, 6)
+        case .Hard:
+            return (6, 8)
+        }
+    }
+    
+    private func numbOfCards()->Int{
+        return Int(cardsPerLevel().column * cardsPerLevel().row)
+    }
+    
+    private func cardSize()->CGSize{
+        let (column, row) = cardsPerLevel()
+        var cardWidth = (screenSize().x  - inset*2 - (column - 1)*space)/column
+        var cardHeight = cardWidth * 1.452
+        if (cardHeight*row + inset*2 + (row - 1)*space) < screenSize().y{
+            return CGSize(width: cardWidth, height: cardHeight)
+        }else{
+            var cardHeight = (screenSize().y  - inset*2 - (row - 1)*space)/row
+            var cardWidth = cardHeight/1.452
+            return CGSize(width: cardWidth, height: cardHeight)
+        }
+    }
+    
+    private func viewSize()->CGSize{
+        let (column, row) = cardsPerLevel()
+        let width = cardSize().width*column + inset*2 + (column - 1)*space
+        let height = cardSize().height*row + inset*2 + (row - 1)*space
+        return CGSize(width: width, height: height)
+    }
     
     
     
