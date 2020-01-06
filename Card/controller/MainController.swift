bdc4e3737231ae117ee672634f6303580522345f
animevn <haanhth21@gmail.com>
2020-01-06 11:33:04 +0700

    edit menu

--- Card/controller/MainController.swift
+++ Card/controller/MainController.swift
@@ -6,9 +6,17 @@ class MainController:UIViewController{
     var level:Level!
     var deck:Deck!
     
+    private let inset:CGFloat = 10
+    private let space:CGFloat = 5
+    private var guess = 0
+    private var openPairs = 0
+    private var cellView:Cell!
+    
     deinit {
         print("The class \(type(of: self)) was remove from memory")
     }
+    
+    
     
     
 }
