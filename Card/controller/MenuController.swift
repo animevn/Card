fc15f0e0915270de43613c860f1814c7aa378790
animevn <haanhth21@gmail.com>
2020-01-06 11:33:27 +0700

    push menu

--- Card/controller/MenuController.swift
+++ Card/controller/MenuController.swift
@@ -6,13 +6,27 @@ class MenuController:UIViewController{
         super.viewDidLoad()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "newgame"{
+            guard let destination = segue.destination as? MainController else{return}
+            destination.modalPresentationStyle = .fullScreen
+        }
+    }
+    
+    private func openGame(level:Level){
+        performSegue(withIdentifier: "newgame", sender: level)
+    }
+    
     @IBAction func bnEasy(_ sender: UIButton) {
+        openGame(level: .Easy)
     }
     
     @IBAction func bnNormal(_ sender: UIButton) {
+        openGame(level: .Normal)
     }
     
     @IBAction func bnHard(_ sender: UIButton) {
+        openGame(level: .Hard)
     }
     
     @IBAction func bnSave(_ sender: UIButton) {
